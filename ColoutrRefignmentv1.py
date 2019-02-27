ba4b3825c82edb82b37bff547e5a4e4f20b6ac37
fpjnijweide <fpjnijweide@protonmail.com>
2019-02-27 14:26:49 +0100

    made function to write as dot file

--- ColoutrRefignmentv1.py
+++ ColoutrRefignmentv1.py
@@ -1,6 +1,7 @@
 from graph import *
 from graph_io import *
 
+
 def CRefignment(filename):
     with open(filename) as f:
         G = load_graph(f)[0][0]
@@ -10,3 +11,7 @@ def CRefignment(filename):
 
 
 
+
+def write_dot_from_graph(G):
+    with open('mygraph.dot', 'w') as f:
+        write_dot(G, f)
