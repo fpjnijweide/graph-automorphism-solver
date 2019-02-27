c8c7437a40e7f7d4fd6e042114146189a304871a
lianvkesteren <lianvkesteren@outlook.com>
2019-02-27 14:37:55 +0100

    versie 2 stubjes

--- ColoutrRefignmentv1.py
+++ ColoutrRefignmentv1.py
@@ -7,6 +7,16 @@ def CRefignment(filename):
         for v in G.vertices:
             v.colornum = v.degree
 
+        old_graph = copy.deepcopy(G)
+        equal = False
+        while not equal:
+            colourGraph(G)
+        return G
 
-
-CRefignment()
+def colourGraph(G):
+    verts = dict()
+    for v in G.vertices:
+        if v.colornum in verts:
+            verts[v.colornum] = verts.get(v.colournum).append(v)
+        else:
+            verts[v.colornum] = [v]
