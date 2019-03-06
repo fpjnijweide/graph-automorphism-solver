db80d3186bee618e2b96f6839103cae12cf5329b
fpjnijweide <fpjnijweide@protonmail.com>
2019-03-06 16:16:32 +0100

    removed deep copying from week4

--- week4.py
+++ week4.py
@@ -3,59 +3,75 @@ from graph_io import *
 from week3 import *
 
 
-def count_isomorphism(G: Graph, H: Graph, D, I):
+def revert_changes(D, I, old_colors):
+    if len(D) != 0:
 
+        for i in range(len(D)):
+            last_D = D[i]
+            last_I = I[i]
+
+            last_D.colornum = old_colors[i]
+            last_I.colornum = old_colors[i]
+            last_D.label = last_D.colornum
+            last_I.label = last_I.colornum
+
+
+def count_isomorphism(G: Graph, H: Graph, D, I):
+    D = D[:]
+    I = I[:]
+    old_colors = []
     if len(D) != 0:
         newcol = len(G.verts)
-        i = len(D) - 1
-        last_D = D[i]
-        last_I = I[i]
+        for i in range(len(D)):
+            last_D = D[i]
+            last_I = I[i]
+            old_colors.append(last_D.colornum)
 
-        last_D.colornum = newcol
-        last_I.colornum = newcol
-        last_D.label = last_D.colornum
-        last_I.label = last_I.colornum
+            last_D.colornum = newcol
+            last_I.colornum = newcol
+            last_D.label = last_D.colornum
+            last_I.label = last_I.colornum
 
+    G = CRefignment(G)
+    H = CRefignment(H)
 
-    new_G=copy.deepcopy(G)
-    new_H=copy.deepcopy(H)
-    new_G = CRefignment(new_G)
-    new_H = CRefignment(new_H)
-
-    if not compare_partitions(new_G, new_H):
+    if not compare_partitions(G, H):
+        revert_changes(D, I, old_colors)
         return 0
     else:
-        all_colors_are_unique=True
-        for i in range(len(new_G.verts)):
-            if len( new_G.verts[i])>1 or len (new_H.verts[i])>1:
-                all_colors_are_unique=False
+        all_colors_are_unique = True
+        for i in range(len(G.verts)):
+            if len(G.verts[i]) > 1 or len(H.verts[i]) > 1:
+                all_colors_are_unique = False
         if all_colors_are_unique:
+            revert_changes(D, I, old_colors)
             return 1
 
-    C=-1
-    for i in range(len(new_G.verts)):
-        Gcolor = new_G.verts[i][:]  # list with vertices of same color
-        Hcolor = new_H.verts[i][:]
-        if len(Gcolor)+len(Hcolor)>=4:
-            C=i
+    C = -1
+    for i in range(len(G.verts)):
+        Gcolor = G.verts[i][:]  # list with vertices of same color
+        Hcolor = H.verts[i][:]
+        if len(Gcolor) + len(Hcolor) >= 4:
+            C = i
             break
 
-    if C==-1:
+    if C == -1:
+        revert_changes(D, I, old_colors)
         return 0
 
-    x=new_G.verts[C][0]
-
+    x = G.verts[C][0]
 
     num = 0
 
-    for y in new_H.verts[C]:
-        num = num + count_isomorphism(new_G, new_H, D + [x], I + [y])
-
+    revert_changes(D, I, old_colors)
+    for y in H.verts[C]:
+        num = num + count_isomorphism(G, H, D + [x], I + [y])
 
     return num
 
+
 if __name__ == "__main__":
-    G1, G2 = load_graphs("graphs/cubes5.grl", 2,3)
+    G1, G2 = load_graphs("graphs/cubes5.grl", 2, 3)
     G1 = initialize_colors(G1)
     G2 = initialize_colors(G2)
     print(count_isomorphism(G1, G2, [], []))
