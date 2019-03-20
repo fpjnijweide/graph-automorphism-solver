5289dcc51114d8d2fe7174429be556d1aa1876c5
fpjnijweide <fpjnijweide@protonmail.com>
2019-03-20 16:35:16 +0100

    removed some useless commented code

--- week4.py
+++ week4.py
@@ -20,14 +20,10 @@ def is_bijection(G: Graph, H:Graph, D: List[int], I: List[int]):
 
 
     return res
-        # for j in range(len(last_D.neighbours)):
-        #     pass
 
 def copy_graph(inputG: Graph):
     G: Graph = copy.copy(inputG)
-
     G._e=[]
-
     G_copied_vertices={}
 
     G._v = inputG._v[:]
@@ -38,33 +34,9 @@ def copy_graph(inputG: Graph):
         G_copied_vertices[inputG._v[i]]=G._v[i]
         G._v[i]._incidence={}
 
-
-    bcdef=1
     for edge in inputG._e:
         newedge=Edge(tail=G_copied_vertices[edge.tail],head=G_copied_vertices[edge.head],weight=edge.weight)
         G.add_edge(newedge)
-    # G._e = inputG._e[:]
-    # G_copied_eges={}
-    # for i in range(len(G._e)):
-    #     G._e[i] = copy.copy(inputG._e[i])
-    #     G._e[i]._head=G_copied_vertices[G._e[i]._head]
-    #     G._e[i]._tail = G_copied_vertices[G._e[i]._tail]
-    #     G_copied_eges[inputG._e[i]]=G._e[i]
-    #
-
-    # for i in range(len(G._v)):
-    #     G._v[i]._incidence = copy.copy(inputG._v[i]._incidence)
-    #     # incidence is a dict that maps vertex -> set of edges
-    #     for vertex, edge_set in list(iter(G._v[i]._incidence.items()))[:]:
-    #         G._v[i]._incidence[G_copied_vertices[vertex]] = G._v[i]._incidence.pop(vertex)
-    #
-    #         for edge in list(iter(edge_set))[:]:
-    #             edge_set.remove(edge)
-    #
-    #             new_edge=G_copied_eges[edge]
-    #             edge_set.add(new_edge)
-    #
-    #         #G._v[i]._incidence[new_key] = dictionary.pop(old_key)
 
     return G
 
@@ -191,7 +163,7 @@ def count_isomorphism_fast(inputG: Graph, inputH: Graph, D, I):
 
 
 if __name__ == "__main__":
-    G1, G2 = load_graphs("graphs/cubes5.grl", 2,3)
+    G1, G2 = load_graphs("graphs/trees36.grl", 3, 5)
     G1 = initialize_colors(G1)
     G2 = initialize_colors(G2)
     G1, G2 = CRefignment(G1, G2)
