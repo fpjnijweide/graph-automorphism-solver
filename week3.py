e518a509809a547f874f9cc5721b950d60cb6e46
fpjnijweide <fpjnijweide@protonmail.com>
2019-03-20 14:34:09 +0100

    attempt at fixing count_isomorphism by including is_bijection, improving copy_graph and adding some showing of dot files

--- week3.py
+++ week3.py
@@ -1,6 +1,7 @@
 from graph import *
 from graph_io import *
 import collections
+from graphviz import render
 
 compare = lambda x, y: collections.Counter(x) == collections.Counter(y)
 
@@ -138,5 +139,7 @@ if __name__ == "__main__":
     G1, G2 = CRefignment(G1, G2)
     write_graph_to_dot_file(G1, "G1")
     write_graph_to_dot_file(G2, "G2")
+    render('dot', 'png', 'graphG1.dot')
+    render('dot', 'png', 'graphG2.dot')
     result = compare_partitions(G1, G2)
     print(result)
